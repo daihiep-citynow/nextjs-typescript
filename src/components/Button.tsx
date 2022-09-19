2ba8acce1ac36c8c5f2941fb7628b631abcc52f7
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2022-09-19 13:45:10 +0700

    chore: test CD

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import styles from "./Button.module.scss";
 
 const Button: NextPage = () => (
-  <div className={styles["button-wrapper"]}>Button</div>
+  <div className={styles["button-wrapper"]}>qq</div>
 );
 
 export default Button;
